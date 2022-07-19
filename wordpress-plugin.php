9262b38b071775313eb85e43229353923dd58f2c
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-07-19 15:30:02 +0200

    Adding additional header details

--- wordpress-plugin.php
+++ wordpress-plugin.php
@@ -1,4 +1,10 @@
 <?php
 /*
  * Plugin Name: WordPress Plugin
+ * Description: This is a simple WordPress plugin.
+ * Version: 1.0.0
+ * Author: John Doe
+ * Author URI: http://jondoe.com
+ * Plugin URI: https://wp-plugin.com/
+ *
  */
